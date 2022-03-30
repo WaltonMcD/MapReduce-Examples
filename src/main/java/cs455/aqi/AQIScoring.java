f5e21c146b5a64fb2d45e7b49d0ce274fdfd184f
WaltonMcDclear <wa1ton21@yahoo.com>
2022-03-29 21:25:17 -0600

    Basic example summing aqi based on join value

--- src/main/java/cs455/aqi/AQIScoring.java
+++ src/main/java/cs455/aqi/AQIScoring.java
@@ -15,8 +15,43 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class AQIScoring {
     
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "aqi total");
+        job.setJarByClass(AQIScoring.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 
-        System.out.println("Basis");
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+        private Text joinText = new Text();
+
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer itr = new StringTokenizer(line, ",");
+            joinText.set(itr.nextToken());
+            int aqi = Integer.parseInt(itr.nextToken());
+            context.write(joinText, new IntWritable(aqi));
+            
+        }
+    }
+
+    public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+        }
+        result.set(sum);
+        context.write(key, result);
+        }
     }
 }
